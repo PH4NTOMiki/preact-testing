d2cae97d0460d46286fe795f1cfafd064ccf2c1d
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2021-01-19 15:21:16 +0100

    initial commit

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import { h } from 'preact';
+import { Link } from 'preact-router/match';
+import style from './style.css';
+
+const Header = () => (
+	<header class={style.header}>
+		<Link href="/"><h1>Preact App</h1></Link>
+		<nav>
+			<Link activeClassName={style.active} href="/">Home</Link>
+			<Link activeClassName={style.active} href="/profile">Me</Link>
+			<Link activeClassName={style.active} href="/profile/miki">Miki</Link>
+		</nav>
+	</header>
+);
+
+export default Header;
